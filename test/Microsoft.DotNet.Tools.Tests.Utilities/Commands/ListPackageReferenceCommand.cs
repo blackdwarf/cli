a732798e80e6a2651165d83160651e98edc23b87
blackdwarf <zlakne@microsoft.com>
2017-03-18 10:06:36 -0700

    Starting test work

---
+++ test/Microsoft.DotNet.Tools.Tests.Utilities/Commands/ListPackageReferenceCommand.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation and contributors. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.DotNet.Cli.Utils;
+
+namespace Microsoft.DotNet.Tools.Test.Utilities
+{
+    public sealed class PackageReferenceCommand : TestCommand
+    {
+        private string _projectName = null;
+
+        public PackageReferenceCommand()
+            : base("dotnet")
+        {
+        }
+
+        public override CommandResult Execute(string args = "")
+        {
+            args = $"list {_projectName} package {args}";
+            return base.ExecuteWithCapturedOutput(args);
+        }
+
+        public ListReferenceCommand WithProject(string projectName)
+        {
+            _projectName = projectName;
+            return this;
+        }
+    }
+}
